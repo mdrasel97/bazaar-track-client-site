ad590424751736f3661a64b4122d1076f60fb648
mdrasel97 <mollarasel972@gmail.com>
2025-07-14 08:30:52 +0600

    verify vendor route added

--- src/routes/VendorRoute.jsx
+++ src/routes/VendorRoute.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import useAuth from "../hooks/useAuth";
+import useUserRole from "../hooks/useUserRole";
+import Loading from "../components/loading/Loading";
+import { Navigate } from "react-router";
 
-const VendorRoute = () => {
-  return <div>vendor route</div>;
+const VendorRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const { role, isLoading } = useUserRole();
+  if (loading || isLoading) {
+    return <Loading />;
+  }
+  if (!user || role !== "vendor") {
+    return (
+      <Navigate
+        state={{ from: location.pathname }}
+        to={"/forbidden"}
+      ></Navigate>
+    );
+  }
+  return children;
 };
 
 export default VendorRoute;
